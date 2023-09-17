483ad2c785a2a50b82bbf5fd21687d3ede91a199
Viacheslav Kozachok <viak@opinov8.com>
2023-09-17 15:18:24 +0300

    Removed trash and polish code

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ import os
 from time import sleep
 from random import randint
 from random import choice
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
 pygame.init()
 
 FPS = 60
 NUMBER_OF_ENEMIES = 5 
 
-window_width = 1280
-window_height = 720
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
 images = './images/' # where all images stored
 music = './music/'   # where all music and sounds stored
 
-wnd = pygame.display.set_mode((window_width, window_height ))
+wnd = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT ))
 
 clock = pygame.time.Clock()
 
@@ -51,21 +57,21 @@ cracks     = pygame.image.load("./images/cracks.png")
 def display_score():
 	score_text_surface = text.render("Score: " + str(score), True, black)
 	score_text_rect    = score_text_surface.get_rect()
-	score_text_rect.right = window_width - 10
+	score_text_rect.right = WINDOW_WIDTH - 10
 	score_text_rect.top   = 10
 	wnd.blit(score_text_surface, score_text_rect)
 
 def display_max_score():
 	score_text_surface = text.render("Max Score: " + str(max_score), True, black)
 	score_text_rect    = score_text_surface.get_rect()
-	score_text_rect.right = window_width - 10
+	score_text_rect.right = WINDOW_WIDTH - 10
 	score_text_rect.top   = 30
 	wnd.blit(score_text_surface, score_text_rect)
 
 def display_defeated_enemies():
 	de_text_surface = text.render("Defeated enemies: " + str(Hero.defeated_enemies), True, black)
 	de_text_rect    = de_text_surface.get_rect()
-	de_text_rect.right = window_width - 10
+	de_text_rect.right = WINDOW_WIDTH - 10
 	de_text_rect.top   = 50
 	wnd.blit(de_text_surface, de_text_rect)
 
@@ -110,18 +116,12 @@ def game_reset(hero):
 		max_score = score
 	score = 0
 	hero.crash()
-
-
-image_width = 200
-car_images = list(map(resize_image, # Function that takes two arguments
-					  car_images,   # First argument will be from this list
-					  [ image_width for i in range(len(car_images)) ] ))	# Generate list that will be the second 
-																	  		# argument is taken from this list  
+  
 				
 
-enemy_images = list(map(resize_image, enemy_images, [ 200 for x in range(len(enemy_images)) ]))
+# enemy_images = list(map(resize_image, enemy_images, [ 200 for x in range(len(enemy_images)) ]))
 
-menu_image = resize_image(menu_image, window_width)
+menu_image = resize_image(menu_image, WINDOW_WIDTH)
 
 
 class Bullet():
@@ -139,7 +139,7 @@ class Bullet():
 		pygame.draw.circle(wnd, black, (self.x, self.y), 5)
 
 	def is_out_of_screen(self):
-		if self.x > window_width:
+		if self.x > WINDOW_WIDTH:
 			return True
 		return False
 
@@ -171,7 +171,7 @@ class Background():
 		self.line_interval  = 150
 		self.line_width     = 300
 		self.line_height    = 50
-		self.lines = [ pygame.Rect(0, int(window_height / 2) - int(self.line_height / 2), self.line_width, self.line_height)]
+		self.lines = [ pygame.Rect(0, int(WINDOW_HEIGHT / 2) - int(self.line_height / 2), self.line_width, self.line_height)]
 
 	def draw_back(self):
 		wnd.fill(grey)
@@ -186,10 +186,10 @@ class Background():
 		self.draw_lines()
 
 	def append_line(self):
-		while self.lines[-1].x < window_width:
+		while self.lines[-1].x < WINDOW_WIDTH:
 			self.lines.append(
 				pygame.Rect(self.lines[-1].x + self.line_width + self.line_interval,
-				int(window_height / 2) - int(self.line_height / 2),
+				int(WINDOW_HEIGHT / 2) - int(self.line_height / 2),
 				self.line_width, self.line_height))
 
 	def pop_line(self):
@@ -227,14 +227,27 @@ class Sound():
 
 class Base():
 	def __init__(self, x, y, images):
+		self.health = 100
+		self.image_width = 200
 		self.x = x
 		self.y = y
 		self.images = images
-		self.rect = images[0].get_rect()
-		self.health = 100
+		self.resize_images()
+		self.rect = self.images[0].get_rect()
+		logger.debug(f"self.rect={self.rect}")
+
+	def resize_images(self):
+		logger.debug(f"self.images={self.images[:4]}")
+		self.images = list(map(resize_image, # Function that takes two arguments
+					  self.images,   # First argument will be from this list
+					  [ self.image_width for i in range(len(self.images)) ] ))	# Generate list that will be the second 
+																	  		# argument is taken from this list
+		logger.debug(f"self.images={self.images[:4]}")
 
 	def draw(self, frame):
-		wnd.blit(self.images[frame], (self.x, self.y))
+		wnd.blit(self.images[frame], (self.x, self.y))	
+
+		# uncomment to debug zone of touch
 		# pygame.draw.rect(wnd, green, self.rect)
 
 	def is_alive(self):
@@ -244,8 +257,14 @@ class Base():
 
 class Hero(Base):
 	defeated_enemies = 0
-	def __init__(self):
-		super().__init__(50, int(window_height / 2), car_images) # x, y, hero_images
+	def __init__(self, type_of_vehicle: str='bus'):
+		if type_of_vehicle == 'car':
+			images = car_images
+		elif type_of_vehicle == 'bus':
+			images = bus_images
+		else:
+			images = car_images
+		super().__init__(50, int(WINDOW_HEIGHT / 2), images) # x, y, hero_images
 		self.rect.size = (self.rect.size[0] - 40, self.rect.size[1] - 20) 
 		s = self.images[0].get_rect().size
 		self.rect.center = (self.x + int(s[0]/2), self.y + int(s[1]/2))
@@ -255,10 +274,10 @@ class Hero(Base):
 	def is_out_of_screen(self):
 		size = self.images[0].get_rect().size
 		# x
-		if self.x < 0 - int(size[0] / 2) or self.x > window_width - int(size[0] / 2):
+		if self.x < 0 - int(size[0] / 2) or self.x > WINDOW_WIDTH - int(size[0] / 2):
 			return True
 		# y
-		if self.y < 0 - int(size[1] / 2) or self.y > window_height - int(size[1] / 2):
+		if self.y < 0 - int(size[1] / 2) or self.y > WINDOW_HEIGHT - int(size[1] / 2):
 			return True
 
 		return False
@@ -271,7 +290,7 @@ class Hero(Base):
 
 	def restore_position(self):
 		self.x = 50
-		self.y = int(window_height / 2)
+		self.y = int(WINDOW_HEIGHT / 2)
 
 	def crash(self):
 		self.restore_position()
@@ -287,8 +306,8 @@ class Enemy(Base):
 	speed = -10
 	def __init__(self):
 		super().__init__(
-			int(window_width * 1.1),    # x
-			int(window_height / 2),     # y
+			int(WINDOW_WIDTH * 1.1),    # x
+			int(WINDOW_HEIGHT / 2),     # y
 			enemy_images)  				# enemy_images
 		self.rect.size = (self.rect.size[0] - 50, self.rect.size[1] - 45) 
 		s = self.images[0].get_rect().size
@@ -304,8 +323,9 @@ class Enemy(Base):
 
 	def restore_position(self):
 		self.health = 100
-		self.x = window_width + randint(200, 600)
-		self.y = randint(0, window_height - self.rect.size[1])
+		self.x = WINDOW_WIDTH + randint(200, 600)
+		print(f"{WINDOW_HEIGHT} - {self.rect.size[1]}")
+		self.y = randint(0, WINDOW_HEIGHT - self.rect.size[1])
 		s = self.images[0].get_rect().size
 		self.rect.center = (self.x + int(s[0]/2), self.y + int(s[1]/2))
 
@@ -313,7 +333,7 @@ class Enemy(Base):
 		self.health -= 50
 
 	def draw(self, frame):
-		if self.x + image_width < 0:
+		if self.x + self.image_width < 0:
 			self.restore_position()
 		super().draw(frame)
 
@@ -351,13 +371,13 @@ def game_menu():
 		textMenu        = pygame.font.Font('./fonts/Sen-Bold.ttf', 140)
 		textMenuSurface = textMenu.render("Main Menu", True, black)
 		textMenuRect    = textMenuSurface.get_rect()
-		textMenuRect.center = (int(window_width/2), int(window_height/6))
+		textMenuRect.center = (int(WINDOW_WIDTH/2), int(WINDOW_HEIGHT/6))
 
 
 		textEnter        = pygame.font.Font('./fonts/Sen-Bold.ttf', text_size) # Get Font
 		textEnterSurface = textEnter.render("Press Enter", True, black) # Get Rendered text
 		textEnterRect    = textEnterSurface.get_rect()
-		textEnterRect.center = (int(window_width/2), int(window_height/1.5)) # Set positions 
+		textEnterRect.center = (int(WINDOW_WIDTH/2), int(WINDOW_HEIGHT/1.5)) # Set positions 
 
 
 		wnd.blit(menu_image, (0,0))
@@ -389,18 +409,18 @@ def game_pause():
 		textMenu        = pygame.font.Font('./fonts/Sen-Bold.ttf', 140)
 		textMenuSurface = textMenu.render("Pause", True, black)
 		textMenuRect    = textMenuSurface.get_rect()
-		textMenuRect.center = (int(window_width/2), int(window_height/4))
+		textMenuRect.center = (int(WINDOW_WIDTH/2), int(WINDOW_HEIGHT/4))
 
 		wnd.blit(textMenuSurface, textMenuRect)
 		pygame.display.update()
 
 def game_loop():
 
-	car = Hero()
+	car = Hero('car')
 	Enemy.enemies = [ Enemy() for _ in range(NUMBER_OF_ENEMIES)]
 
-	x = window_width / 2
-	y = window_height / 2
+	x = WINDOW_WIDTH / 2
+	y = WINDOW_HEIGHT / 2
 
 	vel = 9
 	vel_x = 0
@@ -533,5 +553,5 @@ def game_loop():
 
 
 
-game_menu()
+# game_menu()
 game_loop()
